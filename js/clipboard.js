4ca5208c06811f19a1fb7074dfc58b9aae95e1e2
AnTi3z <anti3z@anti3z.ru>
2018-07-23 18:18:15 +0300

    Save/Load mines functionality. Some tool buttons images.

---
+++ js/clipboard.js
@@ -0,0 +1,40 @@
+function fallbackCopyTextToClipboard(text) {
+  var textArea = document.createElement("textarea");
+  textArea.value = text;
+  document.body.appendChild(textArea);
+  textArea.focus();
+  textArea.select();
+
+  try {
+    var successful = document.execCommand('copy');
+    var msg = successful ? 'successful' : 'unsuccessful';
+    console.log('Fallback: Copying text command was ' + msg);
+  } catch (err) {
+    console.error('Fallback: Oops, unable to copy', err);
+  }
+
+  document.body.removeChild(textArea);
+}
+
+function copyTextToClipboard(text) {
+  if (!navigator.clipboard) {
+    fallbackCopyTextToClipboard(text);
+    return;
+  }
+  navigator.clipboard.writeText(text).then(function() {
+    console.log('Async: Copying to clipboard was successful!');
+  }, function(err) {
+    console.error('Async: Could not copy text: ', err);
+  });
+}
+
+function readTextFromClipboard() {
+   navigator.clipboard.readText()
+  .then(text => {
+    console.log('clipboard:', text);
+    document.getElementById("save_text").value = text;
+  })
+  .catch(err => {
+    console.log('Could not paste text: ', err);
+  });
+}
