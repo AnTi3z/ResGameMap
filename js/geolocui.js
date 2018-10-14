245cd58abb67a4bf6790d91411fe75c5b302d686
AnTi3z <anti3z@anti3z.ru>
2018-10-14 13:05:58 +0300

    Amount of cleanups. Move to Leaflet JS API.

--- js/geolocui.js
+++
@@ -1,9 +0,0 @@
-function GeoLocationControl(geoLocControlDiv, map){
-    geoLocControlDiv.className = 'controls';
-    var controlButton = document.createElement('div');
-    controlButton.className = 'geolocButton';
-
-    geoLocControlDiv.appendChild(controlButton);
-
-    google.maps.event.addDomListener(controlButton, 'click', function() { posGeoloc(map); map.setZoom(16); });
-}
